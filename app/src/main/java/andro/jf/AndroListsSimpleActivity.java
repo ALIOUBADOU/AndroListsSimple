a2ea0fb8662e5a1d6bf3e329ba73a949edc42836
jf <jean-francois.lalande@irisa.fr>
2016-02-23 10:45:45 +0100

    Initial commit

---
+++ app/src/main/java/andro/jf/AndroListsSimpleActivity.java
@@ -0,0 +1,24 @@
+package andro.jf;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class AndroListsSimpleActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        ListView list = (ListView)findViewById(R.id.maliste);
+        ArrayAdapter<String> tableau = new ArrayAdapter<String>(
+                list.getContext(), R.layout.montexte);
+        for (int i=0; i<40; i++) {
+            tableau.add("coucou " + i);
+        }
+        list.setAdapter(tableau);
+        
+    }
+}
